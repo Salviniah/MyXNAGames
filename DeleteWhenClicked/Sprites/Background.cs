32bc64123244dcdd5c9c42f5cda3ced763babbc2
Salviniah <xqwyss1988@gmail.com>
2023-03-07 17:21:14 -0500

    added

---
+++ DeleteWhenClicked/Sprites/Background.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test.Sprites;
+
+public class Background : Sprite
+{
+    private Vector2 _position;
+    public Background(Texture2D texture) : base(texture)
+    {
+        _position = new Vector2(0 + Texture.Width /2 , 0 + Texture.Height /2);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        // _position.X += 5;
+        // if (_position.X > 800)
+        // {
+        //     _position.X = 0;
+        // }
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture,_position,Color.White);
+    }
+}
---
+++ Tutorial012Button/Game1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Tutorial012.Controls;
+
+namespace Tutorial012;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Color _backgroundColour = Color.CornflowerBlue;
+    private List<Component> _GameComponents;
+    
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        var randomButton =
+            new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(0, 0),
+                Text = "0",
+            };
+
+        var quitButton =
+            new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(200, 0),
+                Text = "Quit",
+            };
+        
+        _GameComponents = new List<Component>()
+        {
+            randomButton,
+            quitButton,
+        };
+        
+        randomButton.Click += RandomButton_Click;
+        quitButton.Click += QuitButton_Click;
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+
+    }
+
+    private void RandomButton_Click(object sender, EventArgs e)
+    {
+        var random = new Random();
+        _backgroundColour = new Color(random.Next(0, 255),random.Next(0, 255),random.Next(0, 255));
+    } private void QuitButton_Click(object sender, EventArgs e)
+    {
+        Exit();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        foreach (var gameComponent in _GameComponents)
+        {
+            gameComponent.Update(gameTime);
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(_backgroundColour);
+        _spriteBatch.Begin();
+        foreach (var gameComponent in _GameComponents)
+        {
+            gameComponent.Draw(gameTime,_spriteBatch);
+        }
+        _spriteBatch.End();
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
